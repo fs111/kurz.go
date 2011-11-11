2dd4e2d8e8d23810b5fe0d37cb22529943563110
Andre Kelpe <efeshundertelf@googlemail.com>
2011-11-11 01:30:29 +0100

    working alpha version, that does stores everything in redis - did I mention, that redis rocks? It sure does\!

--- src/kurz.go
+++ src/kurz.go
@@ -1,38 +1,43 @@
 package main
 
 import (
-    "github.com/hoisie/web.go"
+    "web"
     "strings"
     "godis"
+    "fmt"
 )
 
-var counter = 0
+const(
+    SYMBOLS = "0123456789abcdefghijklmnopqrsuvwxyzABCDEFGHIJKLMNOPQRSTUVXYZ"
+    COUNTER = "__counter__"
+    HTTP = "http"
+)
 
-var redis = godis.New("", 0, "")
+var (
+    redis = godis.New("", 0, "")
+)
 
 func resolve(ctx *web.Context, short string) {
     redirect, _ := redis.Get(short)
     ctx.Redirect(302, redirect.String())
 }
 
-func store(ctx *web.Context, url string) string {
-    if ! strings.HasPrefix(url, "http"){
-        url = "http://" + url
+func store(ctx *web.Context, url string){
+    if ! strings.HasPrefix(url, HTTP){
+        url = fmt.Sprintf("%s://%s", HTTP, url)
     }
-    request := ctx.Request
-    counter += 1
-    encoded := encode(counter)
+    ctr, _ := redis.Incr(COUNTER)
+    encoded := encode(ctr)
     redis.Set(encoded, url)
-    return request.Proto + request.Host + "/" + encoded + "\n"
-
+    request := ctx.Request
+    ctx.WriteString( fmt.Sprintf("%s://%s/%s\n", HTTP, request.Host, encoded))
 }
 
 
-func encode(number int) string{
-    const symbols string = "0123456789abcdefghijklmnopqrsuvwxyzABCDEFGHIJKLMNOPQRSTUVXYZ"
-    const base int = len(symbols)
+func encode(number int64) string{
+    const base = int64(len(SYMBOLS))
     rest := number % base
-    result := string(symbols[rest])
+    result := string(SYMBOLS[rest])
     if number - rest != 0{
        newnumber := (number - rest ) / base
        result = encode(newnumber) + result
