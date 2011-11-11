4378a7904a7b36a5c3190c6f1280e74debff73c9
Andre Kelpe <efeshundertelf@googlemail.com>
2011-11-11 01:44:36 +0100

    added some comments and TODOS

--- src/kurz.go
+++ src/kurz.go
@@ -8,20 +8,26 @@ import (
 )
 
 const(
+    // characters used for short-urls
     SYMBOLS = "0123456789abcdefghijklmnopqrsuvwxyzABCDEFGHIJKLMNOPQRSTUVXYZ"
+    // special key in redis, that is our global counter
     COUNTER = "__counter__"
     HTTP = "http"
 )
 
+// connecting to redis on localhost, db with id 0 and no password
 var (
     redis = godis.New("", 0, "")
 )
 
+// function to resolve a shorturl and redirect
 func resolve(ctx *web.Context, short string) {
     redirect, _ := redis.Get(short)
     ctx.Redirect(302, redirect.String())
+    // TODO needs error handling here
 }
 
+// function to shorten and store a url
 func store(ctx *web.Context, url string){
     if ! strings.HasPrefix(url, HTTP){
         url = fmt.Sprintf("%s://%s", HTTP, url)
@@ -33,10 +39,12 @@ func store(ctx *web.Context, url string){
     ctx.WriteString( fmt.Sprintf("%s://%s/%s\n", HTTP, request.Host, encoded))
 }
 
-
+// encodes a number into our *base* representation
+// TODO can this be made better with some bitshifting?
 func encode(number int64) string{
     const base = int64(len(SYMBOLS))
     rest := number % base
+    // strings are a bit weird in go...
     result := string(SYMBOLS[rest])
     if number - rest != 0{
        newnumber := (number - rest ) / base
@@ -45,7 +53,7 @@ func encode(number int64) string{
     return result
 }
 
-
+// main function that inits the routes in web.go
 func main() {
     web.Get("/store/(.*)", store)
     web.Get("/(.*)", resolve)
