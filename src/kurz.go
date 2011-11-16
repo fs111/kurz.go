9a40c60eb32e99bec794c67ec620e74a3b5a221e
Andre Kelpe <efeshundertelf@googlemail.com>
2011-11-16 23:15:07 +0100

    big refactoring of redis storage: * introduce KurzUrl struct type * KurzUrls are now stored as a redis hash * load and store functions present * KurzUrls can be serialized to JSON

--- src/kurz.go
+++ src/kurz.go
@@ -9,6 +9,8 @@ import (
     "url"
     "flag"
     "strconv"
+    "time"
+    "json"
 )
 
 const(
@@ -17,18 +19,45 @@ const(
     HTTP = "http"
 )
 
-// connecting to redis on localhost, db with id 0 and no password
+
 var (
     redis *godis.Client
     config *Config
 )
 
 
+type KurzUrl struct{
+    Key string
+    ShortUrl string
+    LongUrl string
+    CreationDate int64
+    Clicks int64
+}
+
+// Converts the KurzUrl to JSON.
+func (k KurzUrl) Json()[]byte{
+    b, _ := json.Marshal(k)
+    return b
+}
+
+// Creates a new KurzUrl instance. The Given key, shorturl and longurl will
+// be used. Clicks will be set to 0 and CreationDate to time.Nanoseconds()
+func NewKurzUrl(key string, shorturl string, longurl string) *KurzUrl{
+    kurl := new(KurzUrl)
+    kurl.CreationDate = time.Nanoseconds()
+    kurl.Key = key
+    kurl.LongUrl = longurl
+    kurl.ShortUrl = shorturl
+    kurl.Clicks = 0
+    return kurl
+}
+
 
 // function to resolve a shorturl and redirect
 func resolve(ctx *web.Context, short string) {
-    redirect, err := redis.Get(short)
+    redirect, err := redis.Hget(short, "LongUrl")
     if err == nil {
+        go redis.Hincrby(short, "Clicks", 1)
         ctx.Redirect(301, redirect.String())
     } else {
         ctx.Redirect(301, "https://www.youtube.com/watch?v=jRHmvy5eaG4")
@@ -36,7 +65,7 @@ func resolve(ctx *web.Context, short string) {
 }
 
 
-
+// Determines if the string rawurl is a valid URL to be stored.
 func isValidUrl(rawurl string) (u *url.URL, err os.Error){
     if len(rawurl) == 0{
         return nil, os.NewError("empty url")
@@ -48,26 +77,50 @@ func isValidUrl(rawurl string) (u *url.URL, err os.Error){
     return url.Parse(rawurl)
 }
 
+// stores a new KurzUrl for the given key, shorturl and longurl. Existing
+// ones with the same url will be overwritten
+func store(key string, shorturl string, longurl string)*KurzUrl{
+    kurl := NewKurzUrl(key, shorturl, longurl)
+    go redis.Hset(kurl.Key, "LongUrl", kurl.LongUrl)
+    go redis.Hset(kurl.Key, "ShortUrl", kurl.ShortUrl)
+    go redis.Hset(kurl.Key, "CreationDate", kurl.CreationDate)
+    go redis.Hset(kurl.Key, "Clicks", kurl.Clicks)
+    return kurl
+}
+
+// loads a KurzUrl instance for the given key. If the key is
+// not found, os.Error is returned.
+func load(key string) (kurl *KurzUrl, err os.Error){
+    if ok, _ := redis.Hexists(key, "ShortUrl"); ok{
+        kurl := new(KurzUrl)
+        kurl.Key = key
+        reply, _ := redis.Hmget(key, "LongUrl", "ShortUrl", "CreationDate", "Clicks")
+        kurl.LongUrl, kurl.ShortUrl, kurl.CreationDate, kurl.Clicks =
+            reply.Elems[0].Elem.String(), reply.Elems[1].Elem.String(),
+            reply.Elems[2].Elem.Int64(), reply.Elems[3].Elem.Int64()
+        return kurl, nil
+    }
+    return nil, os.NewError("unknown key: " + key )
+}
+
 
 // function to shorten and store a url
 func shorten(ctx *web.Context, data string){
-    const(
-        jsntmpl = "{\"url\" : \"%s\", \"longurl\" : \"%s\"}\n"
-    )
     host := config.GetStringDefault("hostname", "localhost")
     r, _ := ctx.Request.Params["url"]
     theUrl, err := isValidUrl(string(r))
     if err == nil{
         ctr, _ := redis.Incr(COUNTER)
         encoded := Encode(ctr)
-        // fire and forget
-        go redis.Set(encoded, theUrl.Raw)
+        location := fmt.Sprintf("%s://%s/%s", HTTP, host, encoded)
+
+        kurl := store(encoded, location, theUrl.Raw)
 
         ctx.SetHeader("Content-Type", "application/json", true)
-        location := fmt.Sprintf("%s://%s/%s", HTTP, host, encoded)
         ctx.SetHeader("Location", location, true)
         ctx.StartResponse(201)
-        ctx.WriteString(fmt.Sprintf(jsntmpl, location, theUrl.Raw))
+        ctx.Write(kurl.Json())
+        ctx.WriteString("\n")
     }else{
        ctx.Redirect(404, "/")
     }
@@ -81,10 +134,21 @@ func latest(ctx *web.Context, data string){
     }
     c, _ := redis.Get(COUNTER)
     last := c.Int64()
-    for i := last; i > (last - howmany) && i > 0; i -= 1{
-        res, _:= redis.Get(Encode(i))
-        ctx.WriteString(res.String() + "\n")
+    upTo := (last - howmany)
+
+    ctx.SetHeader("Content-Type", "application/json", true)
+    ctx.WriteString("{ \"urls\" : [")
+    for i := last; i > upTo && i > 0; i -= 1{
+        kurl, err := load(Encode(i))
+        if err == nil{
+            ctx.Write(kurl.Json())
+            if i != upTo + 1 {
+                ctx.WriteString(",")
+            }
+        }
     }
+    ctx.WriteString("] }")
+    ctx.WriteString("\n")
 }
 
 
--- stuff/import.sh
+++ stuff/import.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
-HOW_MANY=1000
+HOW_MANY=100000
 
 for site in $(bzcat top-1m.csv.bz2 | head -n $HOW_MANY | awk -F, '{print $2}'); do curl \
  -d"url=$site" http://localhost:9999/shorten/ ; done
