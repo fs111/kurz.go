d1a1eacc85ef1aa11d867393b72cc76bc8747e7b
Andre Kelpe <efeshundertelf@googlemail.com>
2011-12-11 17:54:16 +0100

    added documentation and better error messages to the config parsing code

--- src/config.go
+++ src/config.go
@@ -5,6 +5,7 @@ import (
     "strings"
     "io/ioutil"
     "strconv"
+    "fmt"
 )
 
 
@@ -15,12 +16,15 @@ const (
 )
 
 
+//struct representing a config file
+// which are simple key=value entries
 type Config struct{
     Path string
     Entries map[string]string
 }
 
-
+//creates a new config struct for the given path
+// which is assumed to be a file on the file system
 func NewConfig(path string) *Config{
     cfg := new(Config)
     cfg.Path = path
@@ -28,6 +32,9 @@ func NewConfig(path string) *Config{
     return cfg
 }
 
+
+//Parses the configuration file and stores all key-value
+//pairs in the Entries map
 func (c Config) Parse() os.Error{
     contents, err := ioutil.ReadFile(c.Path)
     if err!= nil{
@@ -45,19 +52,23 @@ func (c Config) Parse() os.Error{
     return nil
 }
 
+
+// Returns the config entry belonging to the given key, if present,
+// otherwise os.Error will be not nil.
 func (c Config) GetString(key string) (entry string, err os.Error){
     value, ok :=  c.Entries[key]
     var e os.Error
     if !ok{
-        e = os.NewError("boom")
+        msg := fmt.Sprintf("unknown configuration entry '%s'", key)
+        e = os.NewError(msg)
     } else {
         e = nil
     }
     return value, e
 }
 
-
-
+//Returns the value for the given key, if it is present, otherwise
+//the val is returned.
 func (c Config) GetStringDefault(key string, val string) string{
     entry, e := c.GetString(key)
     if e == nil{
@@ -66,16 +77,20 @@ func (c Config) GetStringDefault(key string, val string) string{
     return val
 }
 
-
+//Returns the value stored under the key as an int if present, otherwise
+//err is set to non nil
 func (c Config) GetInt(key string) (val int, err os.Error){
     entry, e := c.GetString(key)
     if e == nil {
         return strconv.Atoi(entry)
     }
-    // TODO figure out error handling
-    return -1, os.NewError("eeep")
+    msg := fmt.Sprintf("unknown configuration entry '%s'", key)
+    return -1, os.NewError(msg)
 }
 
+
+// Returns the value stored under key as an int, if no such entry exists
+// it will return the given val.
 func (c Config) GetIntDefault(key string, val int) int{
     entry, e := c.GetInt(key)
     if e == nil {
