9b5d6e6791de3dd0b244b0bf091288d112fe5459
Andre Kelpe <efeshundertelf@googlemail.com>
2012-06-16 22:16:22 +0200

    enable home redirect by default in the bookmarklet example code

--- stuff/bookmarklet.js
+++ stuff/bookmarklet.js
@@ -1,1 +1,1 @@
-javascript:void(location.href='http://localhost:9999/shorten/?url='+encodeURIComponent(location.href))
+javascript:void(location.href='http://localhost:9999/shorten/?url='+encodeURIComponent(location.href)&home=1)
