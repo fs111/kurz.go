363985f5d05caabcfaafdbab6ec25434d1c5364d
Andre Kelpe <efeshundertelf@googlemail.com>
2012-06-16 22:15:18 +0200

    added tweet this link on the home page

--- stuff/assets/js/kurz.js
+++ stuff/assets/js/kurz.js
@@ -13,20 +13,27 @@ function formatURL(url){
     return "<a href=\"" + url +"\">" + clean + "</a>";
 }
 
+function createTweet(url){
+    return "<a target=\"blank\" href=\"https://twitter.com/intent/tweet?text="
+              +  encodeURIComponent(url) + "\">"
+              + "tw</a>";
+}
+
 function loadKurls(howmany){
 
     $('#data tr:not(:first)').remove();
     $.getJSON("/latest/" + howmany, function( allUrls ) { 
         for (var i = 0; i < allUrls.length; i++) {
             var kurl = allUrls[i];
-
             var d = new Date(kurl["CreationDate"] / 1000000);
+
             $("#data").addRow({ 
                 newRow: "<tr>" 
                   + "<td class=\"short\">" + formatURL(kurl["ShortUrl"]) + "</td>"
                   + "<td class=\"long\">" + formatURL(kurl["LongUrl"]) + "</td>"
                   + "<td class=\"date\">" + formatDate(d) + "</td>"
                   + "<td class=\"clicks\">" + kurl["Clicks"] + "</td>"
+                  + "<td class=\"tweet\">" + createTweet(kurl["ShortUrl"]) + "</td>"
                   + "</tr>",
                 rowSpeed: 700
             
