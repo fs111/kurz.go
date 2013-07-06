4ac39291de0a29f9246c72aea0846c20e58a83ae
Andre Kelpe <efeshundertelf@googlemail.com>
2013-07-06 18:22:18 +0200

    fix bookmarklet example

--- stuff/bookmarklet.js
+++ stuff/bookmarklet.js
@@ -1,1 +1,1 @@
-javascript:void(location.href='http://localhost:9999/shorten/?url='+encodeURIComponent(location.href)&home=1)
+javascript:void(location.href='http://localhost:9999/shorten/?url='+encodeURIComponent(location.href)+'&home=1')
